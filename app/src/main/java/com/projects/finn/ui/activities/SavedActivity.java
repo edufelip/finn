897460fca32e73636e0e3383e4fdab91163bbc17
Eduardo Felipe <edu_felip@hotmail.com>
2021-09-22 11:03:14 -0300

    update "saved" activity

--- app/src/main/java/com/projects/finn/ui/activities/SavedActivity.java
+++ app/src/main/java/com/projects/finn/ui/activities/SavedActivity.java
@@ -25,67 +25,36 @@ public class SavedActivity extends AppCompatActivity implements FeedRecyclerAdap
     private ActivitySavedBinding binding;
     private FeedRecyclerAdapter feedRecyclerAdapter;
     private ArrayList<Post> posts;
-    private HandleClick handleClick;
-
-    Post fakepost, fakepost2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySavedBinding.inflate(getLayoutInflater());
-        initializeComponents();
+        initializeRecyclerView();
         setClickListeners();
-
-        fakepost = new Post();
-        fakepost.setId(1);
-        fakepost.setUser_name("Fake Username One");
-        fakepost.setCommunity_title("FakeSubreddit");
-        fakepost.setContent("Fake post title");
-        fakepost.setLikes_count(212);
-        fakepost.setComments_count(14);
-
-        fakepost2 = new Post();
-        fakepost2.setId(12);
-        fakepost2.setUser_name("Faker Username Two");
-        fakepost2.setCommunity_title("SecondFakeSubreddit");
-        fakepost2.setContent("Another fake post title");
-        fakepost2.setLikes_count(12);
-        fakepost2.setComments_count(7);
-
-        posts.add(fakepost);
-        posts.add(fakepost2);
-
-        feedRecyclerAdapter = new FeedRecyclerAdapter(this, posts, this, glide);
-        binding.savedRecyclerview.setAdapter(feedRecyclerAdapter);
-        binding.savedRecyclerview.setLayoutManager(new LinearLayoutManager(this));
 
         setContentView(binding.getRoot());
     }
 
-    public void initializeComponents() {
+    public void initializeRecyclerView() {
         posts = new ArrayList<Post>();
+        feedRecyclerAdapter = new FeedRecyclerAdapter(this, posts, this, glide);
+        binding.savedRecyclerview.setAdapter(feedRecyclerAdapter);
+        binding.savedRecyclerview.setLayoutManager(new LinearLayoutManager(this));
     }
 
     public void setClickListeners() {
-        binding.savedBackButton.setOnClickListener(v -> finish());
-    }
-
-    public void setInterface(HandleClick handle) {
-        this.handleClick = handle;
+        binding.backButton.setOnClickListener(v -> finish());
     }
 
     @Override
     public void onItemClick(int position) {
-        Post post = posts.get(position);
-        Intent intent = new Intent(this, PostActivity.class);
-        intent.putExtra("postId", post.getId());
-        startActivity(intent);
+
     }
 
     @Override
     public void onDeleteClick(int position) {
-        posts.remove(position);
-        feedRecyclerAdapter.notifyItemRemoved(position);
+
     }
 
     @Override
