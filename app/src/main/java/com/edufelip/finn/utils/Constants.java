f568cfb875babee7e5e97d40e2aeb82b34fef498
EduSantos <edu_felip@hotmail.com>
2023-06-29 15:15:55 -0300

    hotfix - update privacy policy url

--- app/src/main/java/com/edufelip/finn/utils/Constants.java
+++ app/src/main/java/com/edufelip/finn/utils/Constants.java
@@ -2,5 +2,5 @@ package com.edufelip.finn.utils;
 
 public class Constants {
     public static int QUERY_PAGE_SIZE = 10;
-    public static String PRIVACY_POLICY_URL = "https://docs.google.com/viewerng/viewer?url=https://portfolio-edufelip.vercel.app/finn_privacy_policy.pdf";
+    public static String PRIVACY_POLICY_URL = "https://portfolio-edufelip.vercel.app/projects/finn/privacy_policy";
 }
